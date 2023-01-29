c9130e3d0a623e07655e5ab3bad2cf863d9a134e
owl64 <89740504+owl64@users.noreply.github.com>
2023-01-29 16:29:20 +0700

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -2,13 +2,13 @@
 
 clear
 echo -e "================================================="
-echo -e "#     Premium Auto Script By Akbar Maulana      #"
+echo -e "#     Premium Auto Script MOD by owl64          #"
 echo -e "================================================="
 echo -e "# For Debian 10 64 bit                          #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
 echo -e "# Build Up By Akbar Maulana                     #"
-echo -e "# MOD By LIZSVR                                 #"
+echo -e "# MOD By owl64                                  #"
 echo -e "================================================="
 echo -e "# Thanks To                                     #"
 echo -e "================================================="
