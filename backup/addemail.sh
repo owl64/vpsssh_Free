7f8fc058bd23a15a8d57457a783ed4873e55b651
owl64 <89740504+owl64@users.noreply.github.com>
2023-01-29 16:33:56 +0700

    Update addemail.sh

--- backup/addemail.sh
+++ backup/addemail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
+# My Telegram : https://t.me/owl64
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -14,8 +14,8 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$(curl https://raw.githubusercontent.com/lizsvr/project/main/ipvps.txt | grep $MYIP | awk '{print $3}')
-if [ $MYIP = $MYIP ]; then
+IZIN=$(curl https://raw.githubusercontent.com/owl64/vpsssh_Free/main/ipvps.txt | grep $MYIP | awk '{print $3}')
+if [ $IZIN = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
